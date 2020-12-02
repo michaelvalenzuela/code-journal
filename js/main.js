11400957d11d75f2beb6d94e40a904f62f5c3a64
michaelvalenzuela <20mvalen@gmail.com>
2020-12-02 15:25:02 -0800

    Populated data model and refactored fullname to fullName

--- js/main.js
+++ js/main.js
@@ -6,5 +6,11 @@ $avatarUrlInput.addEventListener('input', function (e) {
   $avatarImg.setAttribute('src', e.target.value);
 });
 
-$form.addEventListener('submit', function (e) {
+$form.addEventListener('submit', function (event) {
+  data.profile.avatarUrl = event.target.avatarUrl.value;
+  data.profile.username = event.target.username.value;
+  data.profile.fullName = event.target.fullName.value;
+  data.profile.location = event.target.location.value;
+
+  event.preventDefault();
 });
