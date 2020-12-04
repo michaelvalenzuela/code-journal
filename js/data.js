7f1b478b520dcf15d1a70b1ba40c71bd238e4c65
michaelvalenzuela <20mvalen@gmail.com>
2020-12-03 16:32:17 -0800

    Moved storage of local storage data to data.js

--- js/data.js
+++ js/data.js
@@ -11,3 +11,25 @@ var data = {
   },
   entries: []
 };
+
+window.addEventListener('beforeunload', function (e) {
+  const $avatarUrlInput = document.getElementById('avatarUrlInput');
+  const $usernameInput = document.getElementById('usernameInput');
+  const $fullNameInput = document.getElementById('fullNameInput');
+  const $locationInput = document.getElementById('locationInput');
+  const $bioInput = document.getElementById('bioInput');
+
+  if ($avatarUrlInput || $usernameInput || $fullNameInput || $locationInput || $bioInput) {
+    const currentProfile = {
+      avatarUrl: $avatarUrlInput.value,
+      username: $usernameInput.value,
+      fullName: $fullNameInput.value,
+      location: $locationInput.value,
+      bio: $bioInput.value
+    };
+    localStorage.setItem('profile', JSON.stringify([currentProfile]));
+  }
+
+  window.localStorage.setItem('completed-profile', JSON.stringify(data));
+
+});
--- js/main.js
+++ js/main.js
@@ -25,28 +25,6 @@ $form.addEventListener('submit', function (event) {
 
   swapView('profile');
 
-  window.localStorage.setItem('completed-profile', JSON.stringify(data));
-
-});
-
-window.addEventListener('beforeunload', function (e) {
-  const $avatarUrlInput = document.getElementById('avatarUrlInput');
-  const $usernameInput = document.getElementById('usernameInput');
-  const $fullNameInput = document.getElementById('fullNameInput');
-  const $locationInput = document.getElementById('locationInput');
-  const $bioInput = document.getElementById('bioInput');
-
-  if ($avatarUrlInput || $usernameInput || $fullNameInput || $locationInput || $bioInput) {
-    const currentProfile = {
-      avatarUrl: $avatarUrlInput.value,
-      username: $usernameInput.value,
-      fullName: $fullNameInput.value,
-      location: $locationInput.value,
-      bio: $bioInput.value
-    };
-    localStorage.setItem('profile', JSON.stringify([currentProfile]));
-  }
-
 });
 
 function repopulate(previousData) {
